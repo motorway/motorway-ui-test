ad18aec9d3e16fdd59f51c7866cd684adca6fdb8
Abdulsalam Ayinla <ayinlaabdulsalam@gmail.com>
2020-11-02 09:41:05 +0000

    Prettier format codebase

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-const app = require('express')();
-const images = require('./src/images.json');
+const app = require("express")();
+const images = require("./src/images.json");
 
 const randomInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-app.get('/images', ({ query }, res) => {
-  const i = (query.limit) ? images.slice(0, parseInt(query.limit)) : images;
+app.get("/images", ({ query }, res) => {
+  const i = query.limit ? images.slice(0, parseInt(query.limit)) : images;
 
   setTimeout(() => {
     return res.status(200).json(i);
@@ -21,5 +21,5 @@ app.get(/^\/(car-images|avatars)\//, (req, res) => {
 });
 
 app.listen(5000, () => {
-  process.stdout.write('Server is available on http://localhost:5000/\n');
+  process.stdout.write("Server is available on http://localhost:5000/\n");
 });
--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [images, setImages] = useState();
 
   useEffect(() => {
-    fetch('http://localhost:3001/images?limit=10')
-      .then(res => res.json())
-      .then(data => {
-        console.log('Success:', data);
+    fetch("http://localhost:3001/images?limit=10")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success:", data);
         setImages(data);
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
       });
   }, []);
 
   return (
-    <div className='app'>
-      {
-        images && images.map(img => (
-          <div key={img.id} >
-            <img src={img.url} alt=''/>
-            <img src={img.user.profile_image} alt=''/>
+    <div className="app">
+      {images &&
+        images.map((img) => (
+          <div key={img.id}>
+            <img src={img.url} alt="" />
+            <img src={img.user.profile_image} alt="" />
           </div>
-        ))
-      }
+        ))}
     </div>
   );
-}
+};
 
 export default App;
