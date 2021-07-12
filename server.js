951d070f15db2b433b4884767ea9ac505cfe067c
Pradeep Challa <pradeep.challa@centrica.com>
2021-07-13 00:06:03 +0100

    remove random interval for get images api

--- server.js
+++ server.js
@@ -3,16 +3,10 @@
 const app = require('express')();
 const images = require('./src/images.json');
 
-const randomInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
 app.get('/images', ({ query }, res) => {
   const i = (query.limit) ? images.slice(0, parseInt(query.limit)) : images;
 
-  setTimeout(() => {
-    return res.status(200).json(i);
-  }, randomInterval(500, 1500));
+  return res.status(200).json(i);
 });
 
 app.get(/^\/(car-images|avatars)\//, (req, res) => {
