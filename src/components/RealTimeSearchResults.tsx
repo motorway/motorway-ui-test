4f6d63f76581780df5e18353034ceffa6548aedc
Kamil Stepaniak <kjstepaniak1@gmail.com>
2023-03-08 15:43:16 +0000

    chore: cleanup the old files

---
+++ src/components/RealTimeSearchResults.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface RealTimeSearchResultsProps {
+  
+}
+
+const RealTimeSearchResults: React.FC<RealTimeSearchResultsProps> = ({}) => {
+  return (
+    <div className="RealTimeSearchResults">
+      
+    </div>
+  );
+}
+
+export default RealTimeSearchResults;
