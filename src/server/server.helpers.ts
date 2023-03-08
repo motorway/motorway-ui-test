8a451cb8d7dff051349aaae8ca5d861b33186198
Kamil Stepaniak <kjstepaniak1@gmail.com>
2023-03-08 11:37:56 +0000

    feat: replace existing with typescript and new logic

---
+++ src/server/server.helpers.ts
@@ -0,0 +1,17 @@
+import { ImageDTO, Tags } from "../types.js";
+
+const getImagesTags = (images: ImageDTO[]) => images.map(image => image.tags).flat();
+
+const getUniqueTags = (tagsByImage: Tags) => Array.from(new Set(tagsByImage));
+
+export const getAllTags = (images: ImageDTO[]) => {
+  const tagsByImage = getImagesTags(images);
+  return getUniqueTags(tagsByImage);
+} 
+
+export const getFilteredTags = (tags: Tags, queryTag: string) => {
+  if (!queryTag) {
+    return tags;
+  }
+  return tags.filter((tag) => tag.includes(queryTag.toString()))
+}
---
+++ src/server/server.ts
@@ -0,0 +1,38 @@
+import express, { json, Request } from "express";
+import cors from "cors";
+
+import images from './data/images.json' assert { type: 'json' };
+import { getFilteredTags, getAllTags } from "./server.helpers.js";
+
+interface Tags {
+  tag: string;
+}
+
+const port = 8000;
+const feApp = 'http://127.0.0.1:5173';
+
+const allTags = getAllTags(images);
+
+const corsOptions = {
+  origin: feApp,
+  optionsSuccessStatus: 200,
+};
+
+const app = express();
+app.use(cors(corsOptions));
+
+app.get('/api/images', ({ query }, res) => {
+  console.log('query images', query?.input)
+  res.json(images);
+});
+
+app.get('/api/tags', ({ query }:  Request<{}, {}, {}, Tags>, res) => {
+  const { tag: queryTag } = query;
+  const tags = getFilteredTags(allTags, queryTag);
+
+  res.json(tags);
+});
+
+app.listen(port, () => {
+  console.log(`App server listening on port ${port}`);
+});
