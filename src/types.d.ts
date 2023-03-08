3dd00ea1a2fabd22896a16d6c598538b474c0630
Kamil Stepaniak <kjstepaniak1@gmail.com>
2023-03-08 11:37:56 +0000

    feat: replace existing with typescript and new logic

---
+++ src/types.d.ts
@@ -0,0 +1,58 @@
+export type Tags = string[];
+
+export interface ImageDTO {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  color: string;
+  description: null | string;
+  alt_description: string;
+  tags: Tags;
+  likes: number;
+  user: UserDTO;
+  url: string;
+}
+
+export interface Image {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  color: string;
+  description: null | string;
+  altDescription: string;
+  tags: string[];
+  likes: number;
+  user: User;
+  url: string;
+}
+
+export interface UserDTO {
+  id: string;
+  updated_at: string;
+  username: string;
+  name: string;
+  first_name: string;
+  last_name: null | string;
+  bio: string;
+  location: null | string;
+  profile_image: string;
+  total_collections: number;
+  total_likes: number;
+  total_photos: number;
+}
+
+export interface User {
+  id: string;
+  updatedAt: Date;
+  username: string;
+  name: string;
+  firstName: string;
+  lastName: null | string;
+  bio: string;
+  location: null | string;
+  profileImage: string;
+  totalCollections: number;
+  totalLikes: number;
+  totalPhotos: number;
+}
+
