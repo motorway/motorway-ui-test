11f82bf1aa2bfde91ba61fe0d175737637c8e514
Kamil Stepaniak <kjstepaniak1@gmail.com>
2023-03-10 12:16:23 +0000

    feat: add ability to return all tags

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,12 @@ import './App.css'
 /* 
  * Available endpoints
  * http://localhost:8000/api/tags - to return all tags in
- * http://localhost:8000/api/tags?tag=ferrari - to return matching tags
+ * http://localhost:8000/api/tags?tag=fe - to return matching tags
  * http://localhost:8000/api/cars - to return all cars
  * http://localhost:8000/api/cars?tag=ferrari - to return matching cars
  */
 
 function App() {
-
   return (
     <div className="App">
       <h1>Motorway UI challenge</h1>
