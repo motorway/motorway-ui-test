659677f8b56fd419175f748d09171b9934a08cbc
Nikhil Patel <Nikhil.v.p@outlook.com>
2023-05-29 16:08:21 +0100

    Install and use eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
