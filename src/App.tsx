0f05360ab2d3056ef3a541abdae063ac84294367
Nikhil Patel <Nikhil.v.p@outlook.com>
2023-05-29 17:14:12 +0100

    Measure image fetching duration

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React, { useEffect, useState } from "react";
-import CarPage from "./car-page";
+import CarPage from "./car-page/car-page";
 import type { CarImage } from "./types/image";
 
 import "./App.css";
 
 const App = () => {
   const [images, setImages] = useState<CarImage[]>();
+  const [requestDuration, setRequestDuration] = useState<number>();
 
   useEffect(() => {
+    console.log("useEffect");
+    const requestStartTime = performance.now();
+
     fetch("images?limit=10")
       .then((res) => res.json())
       .then((data) => {
         console.log("Success:", data);
         setImages(data);
+        setRequestDuration(Math.trunc(performance.now() - requestStartTime));
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   }, []);
 
-  return <div className="app">{images && <CarPage images={images} />}</div>;
+  return (
+    <div className="app">
+      {images && <CarPage images={images} />}
+      {requestDuration && (
+        <section className="request-duration">
+          Images loaded in {requestDuration} milliseconds
+        </section>
+      )}
+    </div>
+  );
 };
 
 export default App;
