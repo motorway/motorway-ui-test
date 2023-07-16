796deed5f9767526468f946350c0be1ecec0e53c
pauline <pauljdavi.t21@btinternet.com>
2023-07-16 22:52:01 +0100

    added tidy up and to add comments

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,36 +1,4 @@
-// // heror.js
-// import React, { useEffect, useState } from 'react';
 
-// const Hero = () => {
-//   const [images, setImages] = useState();
-
-//   useEffect(() => {
-//     fetch('images?limit=10')
-//       .then(res => res.json())
-//       .then(data => {
-//         console.log('Success:', data);
-//         setImages(data);
-//       })
-//       .catch(error => {
-//         console.error('Error:', error);
-//       });
-//   }, []);
-
-//   return (
-//     <div className='app'>
-//       {
-//         images && images.map(img => (
-//           <div key={img.id}>
-//             <img src={`${img.url}.jpg`} alt='' />
-//             <img src={`${img.user.profile_image}.webp`} alt='' />
-//           </div>
-//         ))
-//       }
-//     </div>
-//   );
-// };
-
-// export default Hero;
 import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './Hero.css';
