e55282ec5f81e958cc24896a2523d26f34513ae9
Kamil Stepaniak <kjstepaniak1@gmail.com>
2023-12-06 16:58:01 +0000

    feat: add mw fonts, adjust spaces and features of the test

--- src/components/task1/Task1.tsx
+++ src/components/task1/Task1.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 
-import SearchInput from '../searchInput/SearchInput';
-
 // Please refer to task 1. Realtime search of readme.md
 const Task1: React.FC = () => {
-  return (
-    <div className="Task1">
-      <SearchInput searchTerm="ferr" />
-    </div>
-  );
+  return <div className="Task1">{/* Add search input here */}</div>;
 };
 
 export default Task1;
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 
+import './styles/setup.scss';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
